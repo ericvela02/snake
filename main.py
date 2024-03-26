88f7209474d396b642663e4786e64b381fa5b873
Eric Vela <79393878+ericvela02@users.noreply.github.com>
2024-03-26 14:38:09 -0400

    Added basic menu and game over screens

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 from snake import *
 from food import Food
 
+
+class States(Enum):
+    MENU = 1
+    GAME = 2
+    GAME_OVER = 3
+
+
 HEIGHT = 800
 WIDTH = 800
 
@@ -17,16 +24,19 @@ food = Food((WIDTH, HEIGHT), BLOCK_SIZE)
 clock = pygame.time.Clock()
 running = True
 
-logo = pygame.image.load('./assets/images/snake-logo.png')
+logo = pygame.image.load("./assets/images/snake-logo.png")
 logo_rect = logo.get_rect()
 logo_width, logo_height = logo_rect.width, logo_rect.height
-background = pygame.image.load('./assets/images/snake-bg.png')
+background = pygame.image.load("./assets/images/snake-bg.png")
+
+state = States.MENU
 
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 snake.steer(Direction.LEFT)
@@ -36,16 +46,43 @@ while running:
                 snake.steer(Direction.UP)
             if event.key == pygame.K_DOWN:
                 snake.steer(Direction.DOWN)
-    
+            if event.key == pygame.K_SPACE:
+                if state == States.GAME_OVER:
+                    state = States.GAME
+                    snake.respawn()
+                    food.respawn()
+                elif state == States.MENU:
+                    state = States.GAME
+
     window.blit(background, (0, 0))
-    
-    snake.move()
-    snake.check_for_food(food)
-    if snake.check_bounds() == True or snake.check_tail_collision() == True:
-        snake.respawn()
-        food.respawn()
-    snake.draw(pygame, window)
-    food.draw(pygame, window)
+
+    # Game state machine
+    if state == States.MENU:
+
+        window.blit(
+            logo,
+            ((WIDTH // 2 - logo_width // 2), (HEIGHT // 2 - logo_height // 2) - 150),
+        )
+
+    elif state == States.GAME:
+
+        snake.move()
+        snake.check_for_food(food)
+        if snake.check_bounds() == True or snake.check_tail_collision() == True:
+            state = States.GAME_OVER
+            continue
+        snake.draw(pygame, window)
+        food.draw(pygame, window)
+
+    elif state == States.GAME_OVER:
+
+        game_over = pygame.font.Font(None, 74).render("Game Over", True, "black")
+        game_over_rect = game_over.get_rect()
+        game_over_width, game_over_height = game_over_rect.width, game_over_rect.height
+        window.blit(
+            game_over,
+            (WIDTH // 2 - game_over_width // 2, HEIGHT // 2 - game_over_height // 2),
+        )
 
     pygame.display.flip()
     clock.tick(5)
